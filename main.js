8dc1befbd1b9252f3db27e881b00fe537413d0f5
andreamazza84 <andrea.mazza84@gmail.com>
2021-01-19 12:44:23 +0100

    compilazione struttura

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+//Primo step: suddivisione sommaria delle aree del sito.
+//Secondo step: individuare le parti che si possono ripetere e implementarle con Vue
+//Terzo step: inserire transizioni/animazioni
+
+Vue.component('button-counter', {
+    data: function(){
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">Cliccato {{count}} volte</button>'
+})
+
+
 let app = new Vue({
-    el: 'root',
+    el: '#root',
     data: {
 
     },
@@ -8,4 +22,5 @@ let app = new Vue({
     },
     created(){},
     mounted(){},
+
 });
