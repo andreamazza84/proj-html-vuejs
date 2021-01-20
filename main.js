3926f3533ad1c39c78dcfc17a9bd66d79e984d03
andreamazza84 <andrea.mazza84@gmail.com>
2021-01-20 15:31:44 +0100

    #main-middle in corso

--- main.js
+++ main.js
@@ -24,11 +24,117 @@ let app = new Vue({
                 description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi placeat, harum at maiores expedita deleniti?',
                 visible: false,
                 },
+                {
+                tag: 'photografy',
+                title: 'how to take better concert pictures in 30 seconds',
+                imgURL: 'blog/img/blog-49.jpg',
+                description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi placeat, harum at maiores expedita deleniti?',
+                visible: false,
+                },
+                {
+                tag: 'photografy',
+                title: 'how to take better concert pictures in 30 seconds',
+                imgURL: 'blog/img/blog-49.jpg',
+                description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi placeat, harum at maiores expedita deleniti?',
+                visible: false,
+                },
+                {
+                tag: 'photografy',
+                title: 'how to take better concert pictures in 30 seconds',
+                imgURL: 'blog/img/blog-49.jpg',
+                description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi placeat, harum at maiores expedita deleniti?',
+                visible: false,
+                },
+                {
+                tag: 'photografy',
+                title: 'how to take better concert pictures in 30 seconds',
+                imgURL: 'blog/img/blog-49.jpg',
+                description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi placeat, harum at maiores expedita deleniti?',
+                visible: false,
+                },
+                {
+                tag: 'photografy',
+                title: 'how to take better concert pictures in 30 seconds',
+                imgURL: 'blog/img/blog-49.jpg',
+                description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi placeat, harum at maiores expedita deleniti?',
+                visible: false,
+                },
             ], 
+        posts: {
+            popular: [
+                {
+                    title: 'simple ways to have a pretty face',
+                    date: '12 Jan 2020',
+                    imgURL: 'blog/img/blog-55.jpg',
+                },
+                {
+                    title: 'simple ways to have a pretty face',
+                    date: '12 Jan 2020',
+                    imgURL: 'blog/img/blog-55.jpg',
+                },
+                {
+                    title: 'simple ways to have a pretty face',
+                    date: '12 Jan 2020',
+                    imgURL: 'blog/img/blog-55.jpg',
+                },
+                {
+                    title: 'simple ways to have a pretty face',
+                    date: '12 Jan 2020',
+                    imgURL: 'blog/img/blog-55.jpg',
+                },
+                {
+                    title: 'simple ways to have a pretty face',
+                    date: '12 Jan 2020',
+                    imgURL: 'blog/img/blog-55.jpg',
+                },
+            ],
+            recent: [
+                {
+                    title: 'main reasons to stop texting and driving',
+                    date: '19 Jan 2020',
+                    imgURL: 'blog/img/blog-65.jpg',
+                },
+                {
+                    title: 'main reasons to stop texting and driving',
+                    date: '19 Jan 2020',
+                    imgURL: 'blog/img/blog-65.jpg',
+                },
+                {
+                    title: 'main reasons to stop texting and driving',
+                    date: '19 Jan 2020',
+                    imgURL: 'blog/img/blog-65.jpg',
+                },
+                {
+                    title: 'main reasons to stop texting and driving',
+                    date: '19 Jan 2020',
+                    imgURL: 'blog/img/blog-65.jpg',
+                },
+                {
+                    title: 'main reasons to stop texting and driving',
+                    date: '19 Jan 2020',
+                    imgURL: 'blog/img/blog-65.jpg',
+                },
+            ],
+            featured: [
+                {
+                    title: 'tips to help you quickly prepare your lunch',
+                    tag: 'recipes',
+                    author: 'John Doe',
+                    author_img: 'blog/img/blog-68.jpg',
+                    author_overview: 'lorem ipsum...'
+                },
+            ]
+        },    
     },
-    methods: {
-
+    computed: {
+        popularpost: function() {
+            return this.posts.popular;
+        },
+        recentpost: function(){
+            return this.posts.recent;
+        }
     },
+    methods: {},
     created(){},
     mounted(){},
 
